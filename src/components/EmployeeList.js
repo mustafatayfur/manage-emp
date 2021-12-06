dd8fcf7f582ca786940fea650d5800eabb63d866
mustafa <mltm0204@gmail.com>
2021-12-06 16:32:43 +0300

    first commit

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,24 @@
+/** @format */
+import { useState } from 
+import Employee from "./Employee";
+
+const EmployeeList = () => {
+  return (
+    <table className='table table-striped table-hover'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Employee />
+      </tbody>
+    </table>
+  );
+};
+
+export default EmployeeList;
