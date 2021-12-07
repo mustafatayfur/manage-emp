59305c9912558bebc6fa18b70cb96e30d1331030
mustafa <mltm0204@gmail.com>
2021-12-07 14:58:53 +0300

    first commit

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,26 +1,35 @@
+/** @format */
 
-
-const Employee = () => {
-
+const Employee = ({ employees }) => {
   return (
-    <tr>
-      <td>Thomas Hardy</td>
-      <td>thomashardy@mail.com</td>
-      <td>89 Chiaroscuro Rd, Portland, USA</td>
-      <td>(171) 555-2222</td>
-      <td>
-        <a href='#editEmployeeModal' className='edit' data-toggle='modal'>
-          <i className='material-icons' data-toggle='tooltip' title='Edit'>
-            &#xE254;
-          </i>
-        </a>
-        <a href='#deleteEmployeeModal' className='delete' data-toggle='modal'>
-          <i className='material-icons' data-toggle='tooltip' title='Delete'>
-            &#xE872;
-          </i>
-        </a>
-      </td>
-    </tr>
+    <>
+      {employees.map((employee, index) => (
+        <tr key={index}>
+          <td>{employee.name}</td>
+          <td>{employee.email}</td>
+          <td>{employee.address}</td>
+          <td>{employee.phone}</td>
+          <td>
+            <a href='#editEmployeeModal' className='edit' data-toggle='modal'>
+              <i className='material-icons' data-toggle='tooltip' title='Edit'>
+                &#xE254;
+              </i>
+            </a>
+            <a
+              href='#deleteEmployeeModal'
+              className='delete'
+              data-toggle='modal'>
+              <i
+                className='material-icons'
+                data-toggle='tooltip'
+                title='Delete'>
+                &#xE872;
+              </i>
+            </a>
+          </td>
+        </tr>
+      ))}
+    </>
   );
 };
 
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -2,7 +2,47 @@
 import { useState } from 'react'
 import Employee from "./Employee";
 
+
 const EmployeeList = () => {
+
+  const [employees, setEmployees] = useState([
+    {
+      id: uuidv4(),
+      name: "Thomas Hardy",
+      email: "thomashardy@mail.com",
+      address: "89 Chiaroscuro Rd, Portland, USA",
+      phone: "(171) 555-2222",
+    },
+    {
+      id: uuidv4(),
+      name: "Dominique Perrier",
+      email: "dominiqueperrier@mail.com",
+      address: "Obere Str. 57, Berlin, Germany",
+      phone: "(313) 555-5735",
+    },
+    {
+      id: uuidv4(),
+      name: "Maria Anders",
+      email: "mariaanders@mail.com",
+      address: "25, rue Lauriston, Paris, France",
+      phone: "(503) 555-9931",
+    },
+    {
+      id: uuidv4(),
+      name: "Fran Wilson",
+      email: "franwilson@mail.com",
+      address: "C/ Araquil, 67, Madrid, Spain",
+      phone: "(204) 619-5731",
+    },
+    {
+      id: uuidv4(),
+      name: "Martin Blank",
+      email: "martinblank@mail.com",
+      address: "Via Monte Bianco 34, Turin, Italy",
+      phone: "(480) 631-2097",
+    },
+  ])
+
   return (
     <table className='table table-striped table-hover'>
       <thead>
@@ -15,7 +55,7 @@ const EmployeeList = () => {
         </tr>
       </thead>
       <tbody>
-        <Employee />
+        <Employee employees={employees} />
       </tbody>
     </table>
   );
