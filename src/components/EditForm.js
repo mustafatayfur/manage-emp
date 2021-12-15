eda13f561aa23c0be128b6d6c8953561161ab5b8
mustafa <mltm0204@gmail.com>
2021-12-15 14:00:33 +0300

    edit codes are added

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -4,16 +4,34 @@ import { Form, Button} from "react-bootstrap";
 import { EmployeeContext } from "../contexts/EmployeeContext";
 import { useContext, useState } from "react";
 
-const EditForm = () => {
+const EditForm = ({theEmployee}) => {
   const { updateEmployee } = useContext(EmployeeContext);
 
+  const employee = theEmployee;
+  const id= employee.id;
+
+  const [name, SetName] = useState(employee.name);
+  const [email, SetEmail] = useState(employee.email);
+  const [address, SetAddress] = useState(employee.address);
+  const [phone, SetPhone] = useState(employee.phone);
+
+   const updatedEmployee = {id, name, email, address, phone };
+
+   const handleSubmit = (e)=>{
+       e.preventDefault();
+       updateEmployee(id, updatedEmployee);
+   }
+
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit} >
       <Form.Group>
         <Form.Control
           type='text'
           placeholder='Name *'
           name='name' 
+          value={name}
+          onChange={(e)=>SetName(e.target.value)}
           required
         />
       </Form.Group>
@@ -22,6 +40,9 @@ const EditForm = () => {
           type='email'
           placeholder='Email *'
           name='email'
+          value={email}
+          onChange={(e)=>SetEmail(e.target.value)}
+
           required
         />
       </Form.Group>
@@ -30,14 +51,19 @@ const EditForm = () => {
           as='textarea'
           placeholder='Address *'
           name='address'
+          value={address}
+          onChange={(e)=>SetAddress(e.target.value)}
           rows={3}
         />
       </Form.Group>
       <Form.Group>
         <Form.Control
-          type='number'
+          type='text'
           placeholder='Phone *'
           name='phone'
+          value={phone}
+          onChange={(e)=>SetPhone(e.target.value)}
+
         />
       </Form.Group>
       <Button variant='success' type='submit' block>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { EmployeeContext } from "../contexts/EmployeeContext";
 import {Button, Modal} from "react-bootstrap";
 import EditForm from "./EditForm";
@@ -11,6 +11,10 @@ const Employee = ({ employee }) => {
 
   const handleClose = ()=> setShow(false);
   const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    handleClose();
+    }, [employee])
 
   return (
     <>
@@ -48,7 +52,7 @@ const Employee = ({ employee }) => {
             </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <EditForm/>
+            <EditForm theEmployee={employee}/>
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
