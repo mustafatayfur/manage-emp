c746dc11e882b0db5fc9c86b16ea01e0899be6cd
mustafa <mltm0204@gmail.com>
2021-12-16 16:08:45 +0300

    ...

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,6 +1,6 @@
 import { useContext, useState, useEffect } from "react";
 import { EmployeeContext } from "../contexts/EmployeeContext";
-import {Button, Modal} from "react-bootstrap";
+import {Button, Modal, OverlayTrigger, Tooltip} from "react-bootstrap";
 import EditForm from "./EditForm";
 
 const Employee = ({ employee }) => {
@@ -23,26 +23,9 @@ const Employee = ({ employee }) => {
           <td>{employee.address}</td>
           <td>{employee.phone}</td>
           <td>
-            <button
-            onClick={handleShow}
-            className='btn text-warning btn-act'
-            data-toggle='modal'>
-              <i className='material-icons' data-toggle='tooltip' title='Edit'>
-                &#xE254;
-              </i>
-            </button>
-            <button
-              onClick={()=> deleteEmployee(employee.id)}
-              href='#deleteEmployeeModal'
-              className='btn text-danger btn-act'
-              data-toggle='modal'>
-              <i
-                className='material-icons'
-                data-toggle='tooltip'
-                title='Delete'>
-                &#xE872;
-              </i>
-            </button>
+            
+            
+            
           </td>
       <Modal show={show} onHide={handleClose}>    
           <Modal.Header className="modal-header" >
