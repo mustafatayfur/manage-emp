f1f08e4a320e7ef0eafac6fd72c8bd0274252565
mustafa <mltm0204@gmail.com>
2021-12-16 19:43:42 +0300

    project is completed

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,84 +1,86 @@
-/** @format */
-
-import { Form, Button } from "react-bootstrap";
-import { EmployeeContext } from "../contexts/EmployeeContext";
-import { useContext, useState } from "react";
+import { Form, Button } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import { useContext, useState } from 'react';
 
 const AddForm = () => {
-  const { addEmployee } = useContext(EmployeeContext);
 
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [address, setAddress] = useState("");
-  // const [phone, setPhone] = useState("");
+    const { dispatch } = useContext(EmployeeContext);
 
-  const [newEmployee, setNewEmployee] = useState({
-    name: "",
-    email: "",
-    address: "",
-    phone: "",
-  });
+/*     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState(""); */
 
-  const {name, email, address, phone} = newEmployee;
+    const [newEmployee, setNewEmployee] = useState({
+        name:"", email:"", address: "", phone: ""
+    })
 
-  const onInputChange = (e)=> {
-      setNewEmployee({...newEmployee, [e.target.name]: e.target.value})
-  }
+    const {name, email, address, phone } = newEmployee;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addEmployee(name, email, address, phone);
-  };
+    const onInputChange = (e) => {
+        setNewEmployee({...newEmployee, [e.target.name]: e.target.value})
+    }
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className='formgroup'>
-        <Form.Control
-          type='text'
-          placeholder='Name *'
-          name='name' //2.metodda name propertilerini ekledi.
-          value={name}
-          // onChange={e => setName(e.target.value)}
-          onChange={(e) => onInputChange(e)}
-          required
-        />
-      </Form.Group>
-      <Form.Group className='formgroup'>
-        <Form.Control
-          type='email'
-          placeholder='Email *'
-          name='email'
-          value={email}
-          // onChange={e => setEmail(e.target.value)}
-          onChange={(e) => onInputChange(e)}
-          required
-        />
-      </Form.Group>
-      <Form.Group className='formgroup'>
-        <Form.Control
-          as='textarea'
-          placeholder='Address *'
-          name='address'
-          value={address}
-          // onChange={e => setAddress(e.target.value)}
-          onChange={(e) => onInputChange(e)}
-          rows={3}
-        />
-      </Form.Group>
-      <Form.Group className='formgroup'>
-        <Form.Control
-          type='number'
-          placeholder='Phone *'
-          name='phone'
-          value={phone}
-          // onChange={e => setPhone(e.target.value)}
-          onChange={(e) => onInputChange(e)}
-        />
-      </Form.Group>
-      <Button variant='success' type='submit' block>
-        Add new employee
-      </Button>
-    </Form>
-  );
-};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //addEmployee(name, email, address, phone)
+        dispatch({type: 'add_employee', employee : {
+            name, email, address, phone
+        }})
+    }
+
+
+    return (
+        <Form onSubmit={handleSubmit}>
+
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    placeholder="Name *"
+                    name="name"
+                    value={name}
+                    onChange={e => onInputChange(e)}
+                    required 
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Control
+                    type="email"
+                    placeholder="Email *"
+                    name="email"
+                    value={email}
+                    onChange={e => onInputChange(e)}
+                    required 
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Control
+                    as="textarea"
+                    placeholder="Address *"
+                    name="address"
+                    value={address}
+                    onChange={e => onInputChange(e)}
+                    rows={3} 
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    placeholder="Phone"
+                    name="phone"
+                    value={phone}
+                    onChange={e => onInputChange(e)}
+                />
+            </Form.Group>
+
+            <Button variant="success" type="submit" block>
+                Add New Employee
+            </Button>
+        </Form>
+    )
+}
+
 export default AddForm;
